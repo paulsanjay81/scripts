878ea0a14aeac1a27f5fbb40a83840e18ba168cb
Sanjay Paul <154652237+paulsanjay81@users.noreply.github.com>
2023-12-23 13:45:59 +0530

    Create speedtest.sh
    speedtest.sh first commit.

---
+++ speedtest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Simple bash script to measure both download and upload speeds of your internet connection using the speedtest-cli tool. 
+
+# Run speed test
+echo "Running speed test..."
+
+download_speed=$(speedtest --simple | awk '/Download/ {print $2}')
+upload_speed=$(speedtest --simple | awk '/Upload/ {print $2}')
+
+# Display results
+echo "Download speed: $download_speed"
+echo "Upload speed: $upload_speed"
